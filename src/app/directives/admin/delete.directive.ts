5d2e3250a46b6616ffad2d74314e4d3490cebb5e
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-01-30 23:24:13 +0300

    update delete directive

--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -2,6 +2,9 @@ import { Directive, ElementRef, Renderer2,HostListener ,Input, Output, EventEmit
 import { MatDialog } from '@angular/material/dialog';
 import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
+import { HttpClientService } from 'src/app/services/common/http-client.service';
+import { AlertifyService, MessageType, Position } from 'src/app/services/admin/alertify.service';
+import { HttpErrorResponse } from '@angular/common/http';
 declare var $:any
 
 @Directive({
@@ -12,8 +15,9 @@ export class DeleteDirective {
   constructor(
     private element:ElementRef,
     private _renderer:Renderer2,
-    private productService:ProductService,
-    public dialog: MatDialog
+    private httpClientService:HttpClientService,
+    public dialog: MatDialog,
+    private alertifyService:AlertifyService
     ) {
       const img  = _renderer.createElement("img");
       img.setAttribute("src","../../../../../assets/delete.png")
@@ -24,22 +28,36 @@ export class DeleteDirective {
      }
 
      @Input() id:number
+     @Input() controller:string
      @Output() callback: EventEmitter<any> = new EventEmitter();
 
      @HostListener("click")
     async onclick(){
       this.openDialog(async () =>{
         const td : HTMLTableCellElement = this.element.nativeElement;
-        await this.productService.delete(this.id);
-        $(td.parentElement).animate({
-          opacity:0,
-          left:"+=50",
-          height:"toogle"
-        },700,() => {
-          this.callback.emit();
+        this.httpClientService.delete({
+          controller:this.controller
+        },this.id).subscribe(data=>{
+          $(td.parentElement).animate({
+            opacity:0,
+            left:"+=50",
+            height:"toogle"
+          },700,() => {
+            this.callback.emit();
+            this.alertifyService.message("Ürün başarıyla silindi.",{
+              dismissOthers:true,
+              messageType:MessageType.Success,
+              position:Position.BottomRight
+            })
+          })
+        },(errorResponse:HttpErrorResponse) =>{
+          this.alertifyService.message("Beklenmeyen bir hata oluştu.",{
+            dismissOthers:true,
+            messageType:MessageType.Error,
+            position:Position.BottomRight
+          })
         })
       });
-
      }
 
      openDialog(afterClosed:any): void {
