899a8e7a2415ccef2cc3154aeb5e7edee179b787
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-01-30 22:18:22 +0300

    add delete confirmation with delete modal dialog

--- src/app/admin/components/products/products.module.ts
+++ src/app/admin/components/products/products.module.ts
@@ -11,6 +11,8 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatTableModule} from '@angular/material/table';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
+import {MatDialogModule} from '@angular/material/dialog';
+import { DeleteDialogComponent } from '../../../dialogs/delete-dialog/delete-dialog.component';
 
 
 
@@ -19,7 +21,8 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     ProductsComponent,
     CreateProductComponent,
     ListProductsComponent,
-    DeleteDirective
+    DeleteDirective,
+    DeleteDialogComponent
   ],
   imports: [
     CommonModule,
@@ -31,7 +34,8 @@ import { DeleteDirective } from 'src/app/directives/admin/delete.directive';
     MatInputModule,
     MatButtonModule,
     MatTableModule,
-    MatPaginatorModule
+    MatPaginatorModule,
+    MatDialogModule
   ]
 })
 export class ProductsModule { }
--- src/app/directives/admin/delete.directive.ts
+++ src/app/directives/admin/delete.directive.ts
@@ -1,4 +1,6 @@
 import { Directive, ElementRef, Renderer2,HostListener ,Input, Output, EventEmitter} from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DeleteDialogComponent, DeleteState } from '../../dialogs/delete-dialog/delete-dialog.component';
 import { ProductService } from 'src/app/services/common/models/product.service';
 declare var $:any
 
@@ -7,9 +9,11 @@ declare var $:any
 })
 export class DeleteDirective {
 
-  constructor(private element:ElementRef,
+  constructor(
+    private element:ElementRef,
     private _renderer:Renderer2,
-    private productService:ProductService
+    private productService:ProductService,
+    public dialog: MatDialog
     ) {
       const img  = _renderer.createElement("img");
       img.setAttribute("src","../../../../../assets/delete.png")
@@ -24,11 +28,30 @@ export class DeleteDirective {
 
      @HostListener("click")
     async onclick(){
-      const td : HTMLTableCellElement = this.element.nativeElement;
-      await this.productService.delete(this.id);
-      $(td.parentElement).fadeOut(2000,() =>{
-        this.callback.emit();
+      this.openDialog(async () =>{
+        const td : HTMLTableCellElement = this.element.nativeElement;
+        await this.productService.delete(this.id);
+        $(td.parentElement).animate({
+          opacity:0,
+          left:"+=50",
+          height:"toogle"
+        },700,() => {
+          this.callback.emit();
+        })
       });
+
      }
+
+     openDialog(afterClosed:any): void {
+      const dialogRef = this.dialog.open(DeleteDialogComponent, {
+        data: DeleteState.Yes
+      });
+  
+      dialogRef.afterClosed().subscribe(result => {
+        if(result == DeleteState.Yes){
+          afterClosed();
+        }
+      });
+    }
 
 }
