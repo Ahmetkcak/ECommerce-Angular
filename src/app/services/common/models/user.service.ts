07e26e2a0a35255ae9427c49ca11d7a3b9441dc3
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-02-22 15:19:36 +0300

    update userService and add token and tokenResponse

--- src/app/services/common/models/user.service.ts
+++ src/app/services/common/models/user.service.ts
@@ -3,13 +3,18 @@ import { Add_User } from 'src/app/contracts/users/add_user';
 import { User } from 'src/app/entities/user';
 import { HttpClientService } from '../http-client.service';
 import { firstValueFrom,Observable } from 'rxjs';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../../ui/custom-toastr.service';
+import { TokenResponse } from 'src/app/contracts/token/tokenResponse';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClientService:HttpClientService) { }
+  constructor(
+    private httpClientService:HttpClientService,
+    private toastr : CustomToastrService
+    ) { }
 
   async add(user:User) : Promise<Add_User>{
     const observable:Observable<Add_User | User> = this.httpClientService.post<Add_User | User>({
@@ -20,12 +25,19 @@ export class UserService {
   }
 
   async login(userNameOrEmail: string, password: string, callBackFunction?: () => void): Promise<void> {
-    const observable: Observable<any> = this.httpClientService.post({
+    const observable: Observable<any | TokenResponse> = this.httpClientService.post<any | TokenResponse>({
       controller: "users",
       action: "login"
-    }, { userNameOrEmail, password })
+    }, {userNameOrEmail,password})
 
-    await firstValueFrom(observable);
+    const tokenResponse : TokenResponse = await firstValueFrom(observable) as TokenResponse;
+    if(tokenResponse){
+      localStorage.setItem("accessToken",tokenResponse.token.accessToken);
+      this.toastr.message("Kullanıcı girişi başarıyla gerçekleşti","Giriş başarılı",{
+        messageType:ToastrMessageType.Success,
+        position:ToastrPosition.BottomRight
+      })
+    }
     callBackFunction();
   }
 }
