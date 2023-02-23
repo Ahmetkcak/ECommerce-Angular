09ead56fecb89eb3e313ef24820fa3bc4c0476f4
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-02-23 18:14:46 +0300

    add auth service and update app component

--- src/app/guards/common/auth.guard.ts
+++ src/app/guards/common/auth.guard.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { Observable } from 'rxjs';
+import { _isAuthenticated } from 'src/app/services/common/auth.service';
 import { CustomToastrService, ToastrMessageType, ToastrPosition } from 'src/app/services/ui/custom-toastr.service';
 
 @Injectable({
@@ -18,20 +19,8 @@ constructor(
 }
 
   canActivate(route: ActivatedRouteSnapshot,state: RouterStateSnapshot) {
-    const token : string = localStorage.getItem("accessToken");
 
-    //const decodeToken : string = this.jwtHelper.decodeToken(token);
-    //const expirationDate : Date = this.jwtHelper.getTokenExpirationDate(token);
-    let expired : boolean;
-
-    try {
-      expired = this.jwtHelper.isTokenExpired(token)
-    } catch {
-      expired = true;
-    }
-
-
-    if(!token || expired){
+    if(!_isAuthenticated){
       this.router.navigate(["login"],{queryParams:{returnUrl:state.url}});
       this.toastr.message("Oturum açmanız gerekiyor.","Yetkisiz erişim",{
         messageType : ToastrMessageType.Warning,
