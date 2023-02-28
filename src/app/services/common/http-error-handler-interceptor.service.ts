b4bb294a6a572f56928a5a89a81a2efb39a861da
Ahmetkcak <ahmet.aliberk07@gmail.com>
2023-02-28 16:16:45 +0300

    add global http error handler

---
+++ src/app/services/common/http-error-handler-interceptor.service.ts
@@ -0,0 +1,55 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest, HttpStatusCode } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { catchError, Observable, of } from 'rxjs';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from '../ui/custom-toastr.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HttpErrorHandlerInterceptorService implements HttpInterceptor{
+
+  constructor(
+    private toastr:CustomToastrService
+  ) { }
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(catchError(error => {
+      switch (error.status) {
+        case HttpStatusCode.Unauthorized:
+          this.toastr.message("Bu işlemi yapmaya yetkiniz yok.","Yetkisiz işlem.",{
+            messageType:ToastrMessageType.Warning,
+            position:ToastrPosition.BottomRight
+          })
+          break;
+
+        case HttpStatusCode.InternalServerError:
+          this.toastr.message("Sunucuya erişilmiyor.","Sunucu hatası.",{
+            messageType:ToastrMessageType.Warning,
+            position:ToastrPosition.BottomRight
+          })
+          break;
+
+        case HttpStatusCode.BadRequest:
+          this.toastr.message("Geçersiz istek yapıldı.","Geçersiz istek.",{
+            messageType:ToastrMessageType.Warning,
+            position:ToastrPosition.BottomRight
+          })
+        break;
+
+        case HttpStatusCode.NotFound:
+          this.toastr.message("Sayfa bulunamadı.","Sayfa bulunamadı.",{
+            messageType:ToastrMessageType.Warning,
+            position:ToastrPosition.BottomRight
+          })
+        break;
+
+        default:
+          this.toastr.message("Beklenmeyen bir hata meydana geldi.","Beklenmeyen hata.",{
+            messageType:ToastrMessageType.Warning,
+            position:ToastrPosition.BottomRight
+          })
+        break;
+      }
+      return of(error);
+    }));
+  }
+}
